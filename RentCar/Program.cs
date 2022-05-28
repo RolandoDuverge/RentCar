2d150509161780b4fee7405f7be471c97a62ceac
Rolando Duverge <arturo1872002@gmail.com>
2022-05-27 22:59:26 -0400

    fix login

--- RentCar/Program.cs
+++ RentCar/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<RentCarContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("RentCarContext") ?? throw new InvalidOperationException("Connection string 'RentCarContext' not found.")));
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -34,9 +34,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+app.UseMiddleware<ErrorHandlerMiddleware>();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<JwtMiddleware>();
 
 app.MapControllers();
 
